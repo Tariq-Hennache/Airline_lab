d1e25285c2de3b60e5ef25a8c26ef19f40cb3dd4
Tariq-Hennache <tariq.hennache@gmail.com>
2022-08-30 15:59:21 +0100

    Passenger properties

--- src/main/java/Passenger.java
+++ src/main/java/Passenger.java
@@ -1,4 +1,12 @@
 public class Passenger {
 
-    private
+    private String name;
+    private int contactNumber;
+    private int id;
+
+    public Passenger(String name, int contactNumber, int id){
+        this.name = name;
+        this.contactNumber = contactNumber;
+        this.id = id;
+    }
 }
