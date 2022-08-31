93bae8a8c1307c798c572aefc4a37a2318c61d0b
Hanqing Huang <h.hanqing@yahoo.com>
2022-08-31 14:18:36 +0100

    edited main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,46 +3,65 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("What functionality do you want to perform?");
-        System.out.println("Please type the number from the options below:");
-        System.out.println("1) Display flight");
-        System.out.println("2) Add flight");
-        System.out.println("3) Cancel flight");
-        System.out.println("4) Add passenger");
-        System.out.println("5) Book seat");
+//        System.out.println("What functionality do you want to perform?");
+//        System.out.println("Please type the number from the options below:");
+//        System.out.println("1) Display flight");
+//        System.out.println("2) Add flight");
+//        System.out.println("3) Cancel flight");
+//        System.out.println("4) Add passenger");
+//        System.out.println("5) Book seat");
+//        System.out.println("6) End session");
         Scanner reader = new Scanner(System.in);
 
         Airline airline = new Airline();
         Flight flight = new Flight("London", 5);
-
+        boolean active = true;
         //while loop
-        int functionality = reader.nextInt();
-        reader.nextLine();
+        while (active) {
+            System.out.println("What functionality do you want to perform?");
+            System.out.println("Please type the number from the options below:");
+            System.out.println("1) Display flight");
+            System.out.println("2) Add flight");
+            System.out.println("3) Cancel flight");
+            System.out.println("4) Add passenger");
+            System.out.println("5) Book seat");
+            System.out.println("6) End session");
+            
+            int functionality = reader.nextInt();
+            reader.nextLine();
 
-        if (functionality == 1){
-            airline.getFlights();
-        } else if (functionality == 2){
-            System.out.println("Destination");
-            String destination = reader.nextLine();
-            System.out.println("id");
-            int id = reader.nextInt();
-            Flight newFlight = new Flight(destination, id);
-            airline.add(newFlight);
-        } else if (functionality == 3){
-            //airline.remove();
-        } else if (functionality == 4){
-            System.out.println("Name");
-            String name = reader.nextLine();
-            System.out.println("Contact number");
-            int number = reader.nextInt();
-            System.out.println("id");
-            int id = reader.nextInt();
-            Passenger passenger = new Passenger(name, number, id);
-            flight.addPassenger(passenger);
-        } else if (functionality == 5){
-            //System.out.println(Flight.bookSeat());
-        } else {
-            System.out.println("Please enter 1 to 5");
+            if (functionality == 1) {
+                System.out.println(airline.getFlights());
+            } else if (functionality == 2) {
+                System.out.println("Destination");
+                String destination = reader.nextLine();
+                System.out.println("id");
+                int id = reader.nextInt();
+                Flight newFlight = new Flight(destination, id);
+                airline.add(newFlight);
+                System.out.println("New flight added to " + destination);
+            } else if (functionality == 3) {
+                //airline.remove();
+                System.out.println("Which flight would you like to cancel?");
+                System.out.println(airline.getFlights());
+                String cancelledFlight = reader.nextLine();
+
+            } else if (functionality == 4) {
+                System.out.println("Name");
+                String name = reader.nextLine();
+                System.out.println("Contact number");
+                int number = reader.nextInt();
+                System.out.println("id");
+                int id = reader.nextInt();
+                Passenger passenger = new Passenger(name, number, id);
+                flight.addPassenger(passenger);
+            } else if (functionality == 5) {
+                //System.out.println(Flight.bookSeat());
+            } else if (functionality == 6) {
+                active = false;
+            } else {
+                System.out.println("Please enter 1 to 5");
+            }
         }
     }
 }
